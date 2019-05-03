b1a1f579279df0e566a69106515ce7b7f407657e
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2019-05-03 17:54:48 +0100

    Recriado UserController, adicionadas rotas, alteração ao modelo Aeronave

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,6 @@ Route::get('/password', function () {return view('welcome');});
 Route::patch('/password', function () {return view('welcome');});
 
 Route::resource('aeronaves', 'AeronaveController')->except(['show']);
-/*
-Route::get('/aeronaves', 'AeronaveController@index')->name('listAeronaves');
-Route::put('/aeronaves/{aeronave}', function () {return view('welcome');});
-Route::get('/aeronaves/{aeronave}/edit', 'AeronaveController@edit');
-Route::get('/aeronaves/create', 'AeronaveController@create');
-Route::post('/aeronaves', function () {return view('welcome');});
-Route::put('/aeronaves/{aeronave}', function () {return view('welcome');});
-Route::delete('/aeronaves/{aeronave}', 'AeronaveController@destroy');
-Route::get('/aeronaves/{aeronave}/pilotos', function () {return view('welcome');});
-Route::post('/aeronaves/{aeronave}/pilotos/{piloto}', function () {return view('welcome');});
-Route::delete('/aeronaves/{aeronave}/pilotos/{piloto}', function () {return view('welcome');});
-Route::get('/aeronaves/{aeronave}/precos_tempos', function () {return view('welcome');});
 
 Route::get('/socios', function () {return view('welcome');});
 Route::get('/socios/{socio}/edit', function () {return view('welcome');});
@@ -49,6 +37,20 @@ Route::patch('/socios/reset_quotas', function () {return view('welcome');});
 Route::patch('/socios/{socio}/ativo', function () {return view('welcome');});
 Route::patch('/socios/desativar_sem_quotas', function () {return view('welcome');});
 Route::post('/socios/{socio}/send_reactivate_mail', function () {return view('welcome');});
+
+
+/*
+Route::get('/aeronaves', 'AeronaveController@index')->name('listAeronaves');
+Route::put('/aeronaves/{aeronave}', function () {return view('welcome');});
+Route::get('/aeronaves/{aeronave}/edit', 'AeronaveController@edit');
+Route::get('/aeronaves/create', 'AeronaveController@create');
+Route::post('/aeronaves', function () {return view('welcome');});
+Route::put('/aeronaves/{aeronave}', function () {return view('welcome');});
+Route::delete('/aeronaves/{aeronave}', 'AeronaveController@destroy');
+Route::get('/aeronaves/{aeronave}/pilotos', function () {return view('welcome');});
+Route::post('/aeronaves/{aeronave}/pilotos/{piloto}', function () {return view('welcome');});
+Route::delete('/aeronaves/{aeronave}/pilotos/{piloto}', function () {return view('welcome');});
+Route::get('/aeronaves/{aeronave}/precos_tempos', function () {return view('welcome');});
 
 Route::get('/pilotos/{piloto}/certificado', function () {return view('welcome');});
 Route::get('/pilotos/{piloto}/licenca', function () {return view('welcome');});
