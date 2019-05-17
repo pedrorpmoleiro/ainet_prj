c3df74406b460d714d38a6e0ec4c4add221e016e
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2019-05-17 19:02:40 +0100

    US11

--- app/Movimento.php
+++ app/Movimento.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movimento extends Model
 {
-    
+
 
     protected $fillable=['data','aeronave','hora_descolagem','hora_aterragem','num_diario','num_servico','piloto_id','num_certificado_piloto'
 ,'validade_certificado_piloto','classe_certificado_piloto','natureza','aerodromo_partida','aerodromo_chegada','num_aterragens','num_descolagens'
@@ -15,7 +15,13 @@ class Movimento extends Model
 'instrutor_id','num_licenca_instrutor','validade_licenca_instrutor','tipo_licenca_instrutor','num_certificado_instrutor','validade_certificado_instrutor'
 ,'classe_certificado_instrutor','preco_voo','tempo_voo','confirmado','num_licenca_piloto','validade_licenca_piloto','tipo_licenca_piloto'];
 
-    public function pilotos(){
-        return $this->belongsToMany('App\User','movimentos','piloto_id');
+    public function piloto()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function instrutor()
+    {
+        return $this->belongsTo('App\User');
     }
 }
--- resources/views/movimentos/list.blade.php
+++ resources/views/movimentos/list.blade.php
@@ -7,40 +7,95 @@
             <div class="card">
                 <div class="card-header">{{ $title }}</div>
                 <div class="card-body">
-                    <a class="btn btn-primary mb-4" href="{{ action('MovimentoController@create') }}">Adicionar Movimento</a>
+                    @can('direcao')
+                        <a class="btn btn-primary mb-4" href="{{ action('MovimentoController@create') }}">Adicionar Movimento</a>
+                    @endcan
                     @if (count($movimentos))
                         @include('movimentos.partials.filtros')
                         <table class="table table-striped">
                             <thead>
                                 <tr>
                                     <th>ID</th>
+                                    <th>Aeronave</th>
                                     <th>Data</th>
                                     <th>Hora Descolagem</th>
                                     <th>Hora Aterragem</th>
-                                    <th>Aeronave</th>
-                                    <th>Piloto</th>
-                                    <th>Ações</th>
+                                    <th>Tempo de Voo</th>
+                                    <th>Natureza</th>
+                                    <th>Nome do Piloto</th>
+                                    <th>Aeródromo Partida</th>
+                                    <th>Aeródromo Chegada</th>
+                                    <th>Nº de Aterragens</th>
+                                    <th>Nº de Descolagens</th>
+                                    <th>Nº Diário</th>
+                                    <th>Nº de Serviço</th>
+                                    <th>Conta-horas Inicial</th>
+                                    <th>Conta-horas Final</th>
+                                    <th>Nº de pessoas a bordo</th>
+                                    <th>Tipo de Instrução</th>
+                                    <th>Nome do Instrutor</th>
+                                    <th>Confirmado</th>
+                                    <th>Observações</th>
+                                    @can('direcao')
+                                        <th>Ações</th>
+                                    @endcan
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach($movimentos as $movimento)
                                     <tr>
                                         <td>{{ $movimento->id }}</td>
+                                        <td>{{ $movimento->aeronave }}</td>
                                         <td>{{ $movimento->data }}</td>
                                         <td>{{ $movimento->hora_descolagem }}</td>
                                         <td>{{ $movimento->hora_aterragem }}</td>
-                                        <td>{{ $movimento->aeronave }}</td>
-                                        <td>{{ $movimento->piloto_id }}</td>
+                                        <td>{{ $movimento->tempo_voo }}</td>
+                                        <td>{{ $movimento->natureza }}</td>
+                                        <td>{{ $movimento->piloto->nome_informal }}</td>
+                                        <td>{{ $movimento->aerodromo_partida }}</td>
+                                        <td>{{ $movimento->aerodromo_chegada }}</td>
+                                        <td>{{ $movimento->num_aterragens }}</td>
+                                        <td>{{ $movimento->num_descolagens }}</td>
+                                        <td>{{ $movimento->num_diario }}</td>
+                                        <td>{{ $movimento->num_servico }}</td>
+                                        <td>{{ $movimento->conta_horas_inicio }}</td>
+                                        <td>{{ $movimento->conta_horas_fim }}</td>
+                                        <td>{{ $movimento->num_pessoas }}</td>
                                         <td>
-                                            <div class="row justify-content-center">
-                                                <a class="btn btn-xs btn-primary mr-1" href="{{ action('MovimentoController@edit', ['movimento' => $movimento->id]) }}">Editar</a>
-                                                <form action="{{ action('MovimentoController@destroy', ['movimento' => $movimento->id]) }}" method="POST" role="form" class="inline">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-xs btn-danger  mr-1">Eliminar</button>
-                                                </form>
-                                            </div>
+                                            @if ($movimento->tipo_instrucao)
+                                                {{ $movimento->tipo_instrucao }}
+                                            @else
+                                                N/A
+                                            @endif
                                         </td>
+                                        <td>
+                                            @if ($movimento->tipo_instrucao)
+                                                {{ $movimento->instrutor->nome_informal }}
+                                            @else
+                                                N/A
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if ($movimento->confirmado == 1)
+                                                Sim
+                                            @else
+                                                Não
+                                            @endif
+                                        </td>
+                                        <td>{{ $movimento->observacoes }}</td>
+
+                                        @can('direcao')
+                                            <td>
+                                                <div class="row justify-content-center">
+                                                    <a class="btn btn-xs btn-primary mr-1" href="{{ action('MovimentoController@edit', ['movimento' => $movimento->id]) }}">Editar</a>
+                                                    <form action="{{ action('MovimentoController@destroy', ['movimento' => $movimento->id]) }}" method="POST" role="form" class="inline">
+                                                        @method('DELETE')
+                                                        @csrf
+                                                        <button type="submit" class="btn btn-xs btn-danger  mr-1">Eliminar</button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        @endcan
                                     </tr>
                                 @endforeach
                             </tbody>
