7a8e725d80ad258699c7c34651775d702c839549
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2019-06-01 15:22:13 +0100

    Redirect para change password através da homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,10 @@ class HomeController extends Controller
      */
     public function index()
     {
+        if (Auth::user()->password_inicial) {
+            return redirect()->action('UserController@alterarPassword');
+        }
+
         return view('home');
     }
 }
